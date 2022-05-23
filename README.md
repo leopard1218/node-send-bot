8dc4354f48519f344df4865dee588251b87b2f76
leopard1218 <snowleopards1218@gmail.com>
2022-05-23 09:07:25 -0400

    first

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# node-send-bot
+# Metamask-Transaction-using-Web3-Node.js
+User can check the balance and do the transaction in his metamask account using Node.js. Mainly backend is created. Api testing can be done using Postman Application
+
+Body for api testing on postman : <br/>
+Request-type : Get<br/>
+Format : json
+1. Transaction : <br/>
+{<br/>
+    "from": user's metamask id,<br/>
+    "to": Another metamask id,<br/>
+    "value": amount,<br/>
+    "gas": gas amount,<br/>
+    "privkey": User's private key of metamask account<br/>
+}
+2. Balance Checking :<br/>
+{<br/>
+    "id": user's metamask id<br/>
+}
---
+++ controller/ropsten.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const Web3 = require('web3');
+const url = 'https://ropsten.infura.io/v3/6bd8fee777ac4a1d88c86bd651a2a3fb'
+
+module.exports = {
+    transaction : async (req,res) => {
+        const web3 = new Web3(url);
+        console.log(
+           `Attempting to make transaction from ${req.body.from} to ${req.body.to}`
+        );
+     
+        const createTransaction = await web3.eth.accounts.signTransaction(
+           {
+              from: req.body.from,
+              to: req.body.to,
+              value: web3.utils.toWei(req.body.value, 'ether'),
+              gas: req.body.gas,
+           },
+           req.body.privkey
+        );
+     
+        // Deploy transaction
+        const createReceipt = await web3.eth.sendSignedTransaction(
+           createTransaction.rawTransaction
+        );
+        console.log(
+           `Transaction successful with hash: ${createReceipt.transactionHash}`
+        );
+        res.status(200).json({message:`Transaction successful with hash: ${createReceipt.transactionHash}`})
+    },
+    balance : async (req,res) => {
+        const web3 = new Web3(url);
+        const balance = web3.utils.fromWei(
+           await web3.eth.getBalance(req.body.id),
+           'ether'
+        );
+     
+        console.log(`The balance of ${req.body.id} is: ${balance} ETH.`);
+        res.status(200).json({balance : balance})
+     }
+}
